c2779a0cce33770f3a5da2ab3ca1feacd9e00c45
Bao Mai <maiduybao@gmail.com>
2017-11-17 00:27:25 -0800

    update ui

--- src/app/signup/signup.component.spec.ts
+++ src/app/signup/signup.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupComponent } from './signup.component';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+class RouterStub {
+  navigateByUrl(url: string) { return url; }
+}
+
+class UserServiceStub {
+  register(signup) { return true;}
+}
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -8,7 +19,11 @@ describe('SignupComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      declarations: [ SignupComponent ],
+      providers: [
+        { provide: UserService, useClass: UserServiceStub },
+        { provide: Router, useClass: RouterStub }
+      ]
     })
     .compileComponents();
   }));
